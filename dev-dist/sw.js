3b1995e595a553d776459c31789f109fd80432c3
Paskal <paskal.droidtv1@gmail.com>
2026-06-14 11:35:51 -0400

    Fix build: retire geoLabel inutilise (TS6133) + menage repo (Logo Misc, PMF orphelin)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h0hcnfkjpk8"
+    "revision": "0.rnt96feu5v4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
